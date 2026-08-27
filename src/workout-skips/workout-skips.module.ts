4c32e575205ba9696d26670d223964503cf8c5b5
Gustavo Karnopp <developer.karnopp@gmail.com>
2026-08-26 22:00:24 -0300

    feat(workout-skips): notifica o coach quando o atleta pula um treino

--- src/workout-skips/workout-skips.module.ts
+++ src/workout-skips/workout-skips.module.ts
@@ -3,9 +3,10 @@ import { WorkoutSkipsService } from './workout-skips.service';
 import { WorkoutSkipsController } from './workout-skips.controller';
 import { StudentsModule } from '../students/students.module';
 import { MessagesModule } from '../messages/messages.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [StudentsModule, MessagesModule],
+  imports: [StudentsModule, MessagesModule, NotificationsModule],
   controllers: [WorkoutSkipsController],
   providers: [WorkoutSkipsService],
   exports: [WorkoutSkipsService],
